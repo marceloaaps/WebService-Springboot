82b0942f6f11f08815b0f42d44ebe9b5907a4572
marceloaaps <marceloalexandreaps@gmail.com>
2024-02-26 10:59:11 -0300

    Initial Commit

---
+++ src/main/java/com/marceloproject/webservicespring/WebServiceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.marceloproject.webservicespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marceloproject/webservicespring/WebServiceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marceloproject.webservicespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
