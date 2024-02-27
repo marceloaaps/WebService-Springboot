d682870c75888d2d95930b96555d1e3d31a510f8
marceloaaps <marceloalexandreaps@gmail.com>
2024-02-27 14:28:24 -0300

    Feita a classe UserResource, para fazer o map e criação de um método findAll.

---
+++ src/main/java/com/marceloproject/webservicespring/Resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.marceloproject.webservicespring.Resources;
+
+import com.marceloproject.webservicespring.entities.User;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Carl", "cj@gmail.com", "222222222", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
