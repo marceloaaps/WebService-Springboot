32155fafa6d85a96400d2aec0bb725fd6d2dcb34
marceloaaps <marceloalexandreaps@gmail.com>
2024-02-28 17:44:02 -0300

    Alterada classe User para transformação em um DB H2, também adicionadas novas configurações no maven e application.properties para inicialização do JPA com H2.

--- src/main/java/com/marceloproject/webservicespring/entities/User.java
+++ src/main/java/com/marceloproject/webservicespring/entities/User.java
@@ -1,9 +1,16 @@
 package com.marceloproject.webservicespring.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
