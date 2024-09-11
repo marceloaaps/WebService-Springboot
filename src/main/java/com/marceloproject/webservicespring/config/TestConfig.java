1bdb42f6e8dd9c23c8418fed91aea2b0bff11793
marceloaaps <marceloalexandreaps@gmail.com>
2024-09-11 06:59:34 -0300

    Criação da class Enum OrderStatus e outras alterações.

--- src/main/java/com/marceloproject/webservicespring/config/TestConfig.java
+++ src/main/java/com/marceloproject/webservicespring/config/TestConfig.java
@@ -3,6 +3,7 @@ package com.marceloproject.webservicespring.config;
 
 import com.marceloproject.webservicespring.entities.Order;
 import com.marceloproject.webservicespring.entities.User;
+import com.marceloproject.webservicespring.entities.enums.OrderStatus;
 import com.marceloproject.webservicespring.repositories.OrderRepository;
 import com.marceloproject.webservicespring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Joaquina", "maria@gmail.com", "988881111", "123456");
         User u2 = new User(null, "Alek Fumasa", "alex@gmail.com", "940028922", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2024-08-21T14:52:01Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2024-08-22T16:53:14Z"), u1);
-        Order o3 = new Order(null, Instant.parse("2024-06-15T21:34:55Z"), u2);
+        Order o1 = new Order(null, Instant.parse("2024-08-21T14:52:01Z"),  OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2024-08-22T16:53:14Z"), OrderStatus.WAITING_PAYMENT, u1);
+        Order o3 = new Order(null, Instant.parse("2024-06-15T21:34:55Z"), OrderStatus.WAITING_PAYMENT, u2);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- src/main/java/com/marceloproject/webservicespring/entities/Order.java
+++ src/main/java/com/marceloproject/webservicespring/entities/Order.java
@@ -2,9 +2,9 @@ package com.marceloproject.webservicespring.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.marceloproject.webservicespring.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
-import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
@@ -20,16 +20,21 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
+
     @ManyToOne
-    @JoinColumn(name="client_id")
+    @JoinColumn(name = "client_id")
     @JsonIgnore
     private User client;
 
-    public Order(){}
+    public Order() {
+    }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -47,6 +52,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
