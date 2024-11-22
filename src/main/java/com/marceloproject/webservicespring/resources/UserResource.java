cd465770e6e82867d000aeab690bba76521a0210
marceloaaps <marceloalexandreaps@gmail.com>
2024-11-22 18:05:06 -0300

    Método update criado para User nas classes UserResource e UserService.

--- src/main/java/com/marceloproject/webservicespring/resources/UserResource.java
+++ src/main/java/com/marceloproject/webservicespring/resources/UserResource.java
@@ -43,4 +43,9 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
 }
