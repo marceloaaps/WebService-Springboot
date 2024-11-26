5d65f24186c2a95067e597d3fd0aa7b633ab24aa
marceloaaps <marceloalexandreaps@gmail.com>
2024-11-26 07:55:16 -0300

    Adicionado Swagger 3 e sua dependência ao projeto.

--- src/main/java/com/marceloproject/webservicespring/WebServiceSpringApplication.java
+++ src/main/java/com/marceloproject/webservicespring/WebServiceSpringApplication.java
@@ -1,11 +1,13 @@
 package com.marceloproject.webservicespring;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class WebServiceSpringApplication {
 
     public static void main(String[] args) {
